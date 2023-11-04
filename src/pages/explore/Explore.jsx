cb8e51ef93ec426ff0cad6abc4210e6258341135
Abhi Patel <abhipatel6788@gmail.com>
2023-11-04 15:34:51 +0530

    fix style

--- src/pages/explore/Explore.jsx
+++ src/pages/explore/Explore.jsx
@@ -38,7 +38,7 @@ const Explore = () => {
   }, [token]);
 
   return (
-    <div className="pt-16 bg-gray-50 h-[85%] overflow-auto">
+    <div className="pt-16 bg-gray-50 h-[90%] overflow-auto">
       {/* <InfiniteScroll
         dataLength={data.length}
         next={fetchData}
